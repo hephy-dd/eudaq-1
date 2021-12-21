1691b6a2718622bc32bb56183ce8f0827eea5e19
Bernhard Pilsl <bernhard.pilsl@cern.ch>
2021-12-21 17:18:16 +0100

    added basic matrix config stuff
    storing and loading to file already works too

--- user/mpw3_gui/configcreator.h
+++ user/mpw3_gui/configcreator.h
@@ -16,12 +16,34 @@ public:
   ~ConfigCreatorView();
 
 private slots:
+  void on_cbSfout_stateChanged(int arg1);
+
+private slots:
+  void on_cbHb_stateChanged(int arg1);
+
+private slots:
+  void on_cbInj_stateChanged(int arg1);
+
+private slots:
+  void on_sbTdac_valueChanged(int arg1);
+
+private slots:
+  void on_cbManOverride_stateChanged(int arg1);
+
+private slots:
+  void on_sbCol_valueChanged(int arg1);
+
+private slots:
+  void on_sbRow_valueChanged(int arg1);
+
+private slots:
+  void on_cbMasked_stateChanged(int arg1);
+
+private slots:
   void on_pbInit_clicked();
   void on_pbParse_clicked();
   void on_pbDeploy_clicked();
   void on_pbClearLog_clicked();
-  void on_pbMisc_toggled(bool checked);
-  void on_pbPower_toggled(bool checked);
 
 private:
   struct ConfigItem {
@@ -33,32 +55,56 @@ private:
 
   struct ConfigPowerItem {
     ConfigPowerItem() {
-      voltage = 0.0;
-      current = 0.0;
+      this->voltage = 0.0;
+      this->current = 0.0;
     }
     ConfigPowerItem(double v, double i = 3.0) {
-      voltage = v;
-      current = i;
+      this->voltage = v;
+      this->current = i;
     }
+
     double voltage;
     double current;
   };
 
-  enum ColumnRole { Item, Comment };
+  struct ConfigPixel {
+    bool masked = false;
+    bool inj = false;
+    bool hb = false;
+    bool sfout = false;
+    int tdac = -1; // -1 means read from calibration file
+  };
+  struct Pixel {
+    int row = -1;
+    int col = -1;
+  };
+
   enum FileSrc { Local, SSH };
 
   Ui::ConfigCreator *ui;
+
+  const int nCol;
+  const int nRow;
 
   QMap<QString, QVariant> mConfigMisc;
   QMap<QString, ConfigPowerItem> mConfigPower;
   QStandardItemModel mModelPower;
   QStandardItemModel mModelMisc;
   QList<ConfigItem> mItems;
+  QList<QList<ConfigPixel *>> mConfigMatrix;
+  QList<Pixel> mPixelToModify;
 
   void parseConfig(const QString &pathToConfig);
   void saveConfig(const QString &fileName);
   void initConfig();
+  void initPixelMatrix();
   void populateModels();
+  void updatePixelInputs();
+  void setPixelToModify();
+  void saveMatrixConfig(const QString &fileName);
+  void loadMatrixConfig(const QString &fileName);
+  ConfigPixel *pixelConfig(int row, int col);
+  ConfigPixel *pixelConfig(const Pixel &pix);
   FileSrc fileSrcFromInput(QString &input);
 };
 
