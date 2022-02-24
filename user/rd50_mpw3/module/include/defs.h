7f8d4dcb9bc37d328b47ee3602ba106eb1e31aa6
Bernhard Pilsl <bernhard.pilsl@cern.ch>
2022-02-24 21:10:16 +0100

    basic setup for MPW3FrameEvent -> MPW3PreprocessedEvent converter done
    not yet tested, but there was a lot of thinking and wrapping my head around stuff,
    I really want to store this stage

--- user/rd50_mpw3/module/include/defs.h
+++ user/rd50_mpw3/module/include/defs.h
@@ -1,3 +1,4 @@
+#include "../tools/Defs.h"
 #include <iostream>
 #include <stdint.h>
 
@@ -6,11 +7,24 @@
 
 namespace DefsMpw3 {
 
+  using word_t = SVD::Defs::VMEData_t;
+  using ts_t = uint64_t;
+
   static constexpr std::size_t dimSensorCol = 64;
   static constexpr std::size_t dimSensorRow = 64;
 
-  /* the following methods are used for extraction of different data in word
-   * from 32bit EOC-word EOC data format:
+  static constexpr ts_t dTPerTsLsb =
+      25; // number of [ns] per LSB in TS_LE and TS_TE
+  static constexpr ts_t dTPerOvflw =
+      dTPerTsLsb *
+      256; // number of [ns] per timestamp-overflow (integer from FPGA)
+  static constexpr ts_t dTSameEvt =
+      6400; // the timewindow in [ns] in which multiple hits are considered to
+            // belong to the same event
+
+  /* the following methods are used for extraction of different data in a 32bit
+   * EOC-word
+   * EOC data format:
    *
    * |31  29 | 28    24 |   23   | 22    16 | 15  8 | 7   0 |
    * |3'b000 | EOC_ADDR | 1'b1/0 | PIX_ADDR | TS_TE | TS_LE |
@@ -18,19 +32,29 @@ namespace DefsMpw3 {
    * bit 23 might be 1 or 0 to destinguish between base and piggy board
    */
 
-  int inline extractDcol(uint32_t word) { // EOC_ADDR
+  uint32_t inline extractDcol(word_t word) { // EOC_ADDR
     return (word & (0x1F << 24)) >> 24;
   };
-  int inline extractPix(uint32_t word) { // PIX_ADDR
+  uint32_t inline extractPix(word_t word) { // PIX_ADDR
     return (word & (0x7F << 16)) >> 16;
   };
-  int inline extractTsTe(uint32_t word) { // TS_TE
+  uint32_t inline extractTsTe(word_t word) { // TS_TE
     return (word & (0xFF << 8)) >> 8;
   };
-  int inline extractTsLe(uint32_t word) { return word & 0xFF; }; // TS_LE
-  int inline extractPiggy(uint32_t word) {
+  uint32_t inline extractTsLe(word_t word) { return word & 0xFF; }; // TS_LE
+  uint32_t inline extractPiggy(word_t word) {
     return (word & (1 << 23)) >> 23;
   }; // piggy or base
+  uint32_t inline extractOverFlowCnt(word_t word) {
+    return word & ~(0x1FF << 23);
+  }
+
+  bool inline isSOF(word_t word) {
+    return (word & (0xAF << 24)) > 0;
+  } // is given word Start Of Frame?
+  bool inline isEOF(word_t word) {
+    return (word & (0xE0 << 24)) > 0;
+  } // is given word End Of Frame?
 
 } // namespace DefsMpw3
 
---
+++ user/rd50_mpw3/module/include/mpw3FileReader.h
@@ -0,0 +1,37 @@
+#ifndef MPW3FILEREADER_H
+
+#include "defs.h"
+#include "eudaq/FileDeserializer.hh"
+#include "eudaq/FileReader.hh"
+
+class Mpw3FileReader : public eudaq::FileReader {
+public:
+  Mpw3FileReader(const std::string &filename);
+  eudaq::EventSPC GetNextEvent() override;
+
+private:
+  struct Hit {
+    uint32_t ovflwSOF, ovflwEOF;
+    double avgFrameOvflw;
+    uint8_t dcol;
+    uint8_t pix;
+    uint8_t tsLe;
+    uint8_t tsTe;
+    DefsMpw3::ts_t globalTs;
+  };
+  using PrefabEvt = std::vector<Hit>;
+
+  static constexpr int sizeEvtQueue = 3;
+
+  void processFrame(const eudaq::EventUP &frame);
+  void finalizePrefab(const PrefabEvt &prefab);
+
+  std::unique_ptr<eudaq::FileDeserializer> mDes;
+  std::string mFilename;
+
+  std::vector<eudaq::EventSP> mTimeStampedEvents;
+  std::vector<Hit> mHitBuffer;
+};
+#define MPW3FILEREADER_H
+
+#endif // MPW3FILEREADER_H
